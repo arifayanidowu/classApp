a8ba9551c075b23115ee4c70d37e2619713b6f8c
stizzle123 <arifayanidowu2@yahoo.com>
2017-11-16 16:30:37 +0100

    all the changes in the world

--- www/includes/header.php
+++ www/includes/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title><?php echo $page_title; ?> </title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<section>
--- www/register.php
+++ www/register.php
@@ -2,9 +2,13 @@
 	
 	$page_title = "Register";
 
-	include('includes/db.php');
-	
 	include('includes/header.php');
+
+	include('includes/db.php');
+
+	include('includes/function.php');
+	
+
 
 	
 	$error = [];
@@ -30,23 +34,12 @@
 		if(empty($error)){
 			$clean = array_map('trim', $_POST);
 
-			$hash = password_hash($clean['password'], PASSWORD_BCRYPT);
-
-			$stmt = $conn->prepare("INSERT INTO admin(firstName, lastName, email,
-				hash) VALUES(:f, :l, :e, :h)"); 
-
-			$data = [
-				":f" => $clean['fname'],
-				":l" => $clean['lname'],
-				":e" => $clean['email'],
-				":h" => $hash
-			];
-
-			$stmt->execute($data);
+			doAdminRegister($conn, $clean);
 		}
 
 	}
 ?>
+
 <div class="wrapper">
 		<h1 id="register-label">Register</h1>
 		<hr>
