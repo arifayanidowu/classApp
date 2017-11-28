0904908ad3e8796220a4220c4cc8e7cceb6e4472
stizzle123 <arifayanidowu2@yahoo.com>
2017-11-28 15:13:44 +0100

    dbconn.php

--- www/products/static.php
+++ www/products/static.php
@@ -2,6 +2,7 @@
 	// Anything that is Static does not really belong to the object, you cannot use $this inside a static object
 	class Statics { // If a method is static, you don't need to create an instance of the class to call it...
 	// before you create a class, create an instance of that class
+	// Utility functions should always be static, meaning any object that defines something about an object
 
 		private static $_hourlyRate = 50; // private static count is a class member, statics are class members cos they are not copied and they have one copy of them
 
@@ -22,11 +23,19 @@
 
 		public static function doStatic($myname){
 
-			$this->name = $myname; // Everytime you create an object of a static it becomes a new instance, you cannot be doing object related activity inside a none object context...so DO NOT USE $this to reference it,
+			/*$this -> $name = $myname;*/ // Everytime you create an object of a static it becomes a new instance, you cannot be doing object related activity inside a none object context...so DO NOT USE $this to reference it,
 
 		}
 	}
 
+
+		class A {
+
+			const HOURLY_RATE = 50;
+		}
+
+
+		echo A::HOURLY_RATE;
 
 	//$staticInstance = new Statics(); // Everything you call the Static class you implement count
 
